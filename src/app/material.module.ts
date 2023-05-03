6c49352c6b6812438b54ae621bf8d05246aea520
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2023-05-03 12:04:54 +0300

    feat: footer implementation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,8 +4,17 @@ import { CommonModule } from '@angular/common'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
-  exports: [CommonModule, MatInputModule, MatFormFieldModule, MatSelectModule],
+  exports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
