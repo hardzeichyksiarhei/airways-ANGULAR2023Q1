747645796a86d539c6a10243108398eeaed6e425
YaroshUlad <yarosh.uladzimir@gmail.com>
2023-05-25 16:58:44 +0300

    feat: add dialog

--- src/app/airways/components/header/header.component.ts
+++ src/app/airways/components/header/header.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { AuthDialogComponent } from '../auth-dialog/auth-dialog.component'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ): void {
+    this.dialog.open(AuthDialogComponent, {
+      width: '514px',
+      ariaModal: true,
+      enterAnimationDuration,
+      exitAnimationDuration,
+    })
+  }
+}
