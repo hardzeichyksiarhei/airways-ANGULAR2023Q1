0a7d8d50d286496affb3684faea7e02c2c07ba52
YaroshUlad <yarosh.uladzimir@gmail.com>
2023-05-26 14:16:03 +0300

    feat: change AirwaysState

--- src/app/airways/store/types.ts
+++ src/app/airways/store/types.ts
@@ -1,9 +1,11 @@
 import { AirportsState } from './airports/airports.reducer'
 import { SearchState } from './search/search.reducer'
 import { SettingsState } from './settings/settings.reducer'
+import { AuthState } from './auth/auth.reducer'
 
 export interface AirwayState {
   settings: SettingsState
   airports: AirportsState
   search: SearchState
+  auth: AuthState
 }
