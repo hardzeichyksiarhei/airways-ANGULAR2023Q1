9f02e675bee8ae33b8484f7ed41d45cda8adeb87
YaroshUlad <yarosh.uladzimir@gmail.com>
2023-05-26 19:44:38 +0300

    feat: create booking-process page template

--- src/app/airways/pages/booking-process/booking-process.component.ts
+++ src/app/airways/pages/booking-process/booking-process.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { ActivatedRoute, Router } from '@angular/router'
+import { selectSearchFeature } from '../../store/search/search.selectors'
+import { take } from 'rxjs'
 
 @Component({
   selector: 'app-booking-process',
   templateUrl: './booking-process.component.html',
   styleUrls: ['./booking-process.component.scss'],
 })
-export class BookingProcessComponent {}
+export class BookingProcessComponent {
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  clickToContinue() {
+    this.router.navigate(['summary'], {})
+  }
+
+  clickToBack() {
+    this.store
+      .select(selectSearchFeature)
+      .pipe(take(1))
+      .subscribe(({ search }) => {
+        this.router.navigate(['selection'], {
+          queryParams: {
+            fromKey: search.route.from?.key,
+            toKey: search.route.to?.key,
+            forwardDate: search.dates.start?.toISOString(),
+            backDate: search.dates.end?.toISOString(),
+          },
+        })
+      })
+    // this.router.navigate(['selection'], {})
+  }
+}
